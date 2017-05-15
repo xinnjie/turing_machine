20919e000fe57f59dc2057b2521f37292b647e2f
xinnjie <capitalg_31@163.com>
2017-05-15 17:34:14 +0800

    add the step_forward functionality add class Tape, witch resemble the infinite long tape in turing machine

--- turing_machine.py
+++ turing_machine.py
@@ -1,15 +1,20 @@
 import re
 
 class TuringMachine:
+	direction = {'R':1, 'r':1, 'S':0, 's':0, 'L':-1, 'l':-1}
+
+
 	def __init__(self, states:set, start_state:str, terminating_states:set, transforming_funcs_string:str,
-	             blank_symbol:set='B', tape_symbols:set=None, input_letters:set=None):
+	             blank_symbol:set='B', tape_symbols:set=None, input_letters:set=None, tape:str=None):
 		self.states = states
 		self.start_state = start_state
 		self.terminate_states = terminating_states
-		self.tranform_funcs_string = transforming_funcs_string
+		self.transform_funcs_raw_string = transforming_funcs_string
 		self.blank_symbol = blank_symbol
 		self.tape_symbols = tape_symbols
 		self.input_letters = input_letters
+		self.tape = tape
+		self.current_state = start_state
 
 		self.func_pattern = re.compile(r"""(?P<func>\w+)
 										\(
@@ -26,6 +31,56 @@ class TuringMachine:
 										(?P<direction>[RrLlSs])
 										\)""", re.VERBOSE)
 
+	@property
+	def tape(self):
+		try:
+			return self._tape
+		except AttributeError:
+			self._tape = None
+		return self._tape
+
+	@tape.setter
+	def tape(self, value: str):
+		if not value:
+			return
+		if self.tape_symbols:
+			for letter in value:
+				if letter not in self.tape_symbols:
+					raise ConstructionError('illegal input tape ' + letter + ' not allowed')
+		else:
+			tape_symbols = set()
+			for letter in value:
+				tape_symbols.add(letter)
+				self.tape_symbols = tape_symbols
+		self._tape = value
+
+	@property
+	def tape_symbols(self):
+		try:
+			return self._tape_symbols
+		except AttributeError:
+			self._tape_symbols = None
+		return self._tape_symbols
+
+	@tape_symbols.setter
+	def tape_symbols(self, value):
+		self._tape_symbols = value
+
+	@property
+	def position(self):
+		try:
+			return self._position
+		except AttributeError:
+			self._position = 0
+		return self._position
+
+	@position.setter
+	def position(self, value: int):
+		if value < 0:   # todo 为方便起见，有可能到达-1，指向的字母是 空，这里再做打算
+			raise IndexError("position index out of range, position can not be "+str(value))
+		self._position = value
+
+
 	def generate_transforming_funcs(self, funcs:str):
 		# 去掉空格
 		funcs = self.clean_func_str(funcs)
@@ -40,7 +95,8 @@ class TuringMachine:
 			transform_funcs[argument] = return_value   # todo 做检查
 		return transform_funcs
 
-	def clean_func_str(self, funcs_str:str) -> list:
+	@classmethod
+	def clean_func_str(cls, funcs_str:str) -> list:
 		after_clean = funcs_str.translate(str.maketrans({'\t':'', '\n':'', ' ':''}))   # 删去多余的空白符
 		return after_clean.split(';')
 
@@ -49,8 +105,42 @@ class TuringMachine:
 		try:
 			return self._transform_funcs
 		except AttributeError:
-			self._transform_funcs = self.generate_transforming_funcs(self.tranform_funcs_string)
-			return self._transform_funcs
+			self._transform_funcs = self.generate_transforming_funcs(self.transform_funcs_raw_string)
+
+		return self._transform_funcs
+
+	def _step_forward(self):
+		tape = self.tape
+		if self.current_state in self.terminate_states:
+			raise HaltException
+		read_letter = tape[self.position]
+		# tuple(starte_state, read_letter) -> tuple(to_state, write_letter, direction)
+		next_step = self.transform_funcs[(self.current_state ,read_letter)]
+		self.current_state, tape[self.position], direction= next_step
+		self.position = self.position + self.__class__.direction[direction]
+
+	def step_forward(self, steps=1) -> list:
+		import copy
+		process = []
+		try:
+			for _ in range(steps):
+				self._step_forward()
+				process.append(copy.deepcopy(self.tape))
+		except HaltException:
+			pass
+		return process
+
+
+	def run(self) -> bool:
+		try:
+			for _ in range(1000):
+				self._step_forward()
+		except HaltException:
+			return True
+
+		return False
+
+
 '''
 	@property
 	def start_state(self):
@@ -68,6 +158,10 @@ class TuringMachine:
 
 
 
+class Tape:
+	def __init__(self, string:str):
+		self.string = string
+		self.length = len(string)
 
 
 
@@ -76,9 +170,15 @@ class ConstructionError(Exception):
 	pass
 
 
+class HaltException(Exception):
+	pass
+
 import unittest
+
+
 class MachineTest(unittest.TestCase):
 	def setUp(self):
+		# 该自动机要求输入带包含3个以上1
 		states = {'q0', 'q1', 'q2', 'q3'}
 		ter_states = {'q3'}
 		start_state = 'q0'
@@ -91,14 +191,29 @@ class MachineTest(unittest.TestCase):
 
 		self.tm = TuringMachine(states, start_state, ter_states, trans_funcs)
 
-	def test_match_function(self):
+	def test_match_function(self):  # matching functionality
 		result = self.tm.func_pattern.match('f(q0,0)=(q0,0,R)')
-		print(result.groupdict())
-		self.assertIsNotNone(self.tm.func_pattern.match('f(q0,0)=(q0,0,R)'))
+		self.assertEqual(result.group('func'), 'f')
+		self.assertEqual(result.group('direction'), 'R')
+		self.assertEqual(result.group('write_letter'), '0')
 
 	def test_transform_funcs(self):
 		funcs = self.tm.transform_funcs
 		print(funcs)
 		self.assertIsNotNone(funcs)
 
+	def test_tape(self):
+		tape = '11001101'
+		self.tm.tape = tape
+		self.assertEqual(self.tm.tape, tape)
+		# illegal tape input
+		try:
+			self.tm.tape = '121'
+		except ConstructionError:
+			# 自动机不应该接受 ‘121’这个纸带输入
+			self.assertEqual(self.tm.tape, tape)
+
+
+	def test_turing_machine_functionality(self):
+		self.assertTrue(self.tm.run())
 
